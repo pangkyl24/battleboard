340675e274c890244863ce0ea0dda19cc530c7a2
vijmanan04 <75040859+vijmanan04@users.noreply.github.com>
2021-10-14 09:35:56 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,12 +3,22 @@
 *
 */
 
-public class Main {
+public class Battle {
 
 public static void main ( String[] args) {
+	Board playerShips = new Board();
+	Board playerHits = new Board();
+	playerHits.showBoard();
+	System.out.println("+--+--+--+--+--+--+--+ ");
+	playerShips.showBoard();
+	System.out.println("");
+	int arr[] = {1,1,1,5};
+	playerShips.shipLocation(arr);
+	playerHits.showBoard();
+	System.out.println("+--+--+--+--+--+--+--+ ");
+	playerShips.showBoard();
 
 
 }  //close main method
 
-} . //close the Main class
-
+}  //close the Main class
