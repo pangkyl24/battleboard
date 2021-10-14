1dcb21b6b17ae2ca23e570a3363298c276440238
pangkyl24 <77285931+pangkyl24@users.noreply.github.com>
2021-10-14 09:08:30 -0400

    Update Ship.java

--- Ship.java
+++ Ship.java
@@ -1,16 +1,26 @@
-public class Ship {
+public class Ship{
 
 	int shipSize;
+	int[] shipStartEnd;
 	
-	public Ship(){
+	public Ship(){ //CONSTRUCTOR
 		
 		shipSize = 2;
+		shipStartEnd = new int[4]; // (startX, startY, endX, endY)
 		
 	}
 	
-	public Ship(int size){
+	public Ship(int size){ //OVERLOADED CONTRUCTOR
 		
 		shipSize = size;
+		shipStartEnd = new int[4];
+		
+	}
+	
+	public int[] shipPos() {
+		
+		return shipStartEnd;
+		
 		
 	}
 
