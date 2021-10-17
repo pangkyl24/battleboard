bca9e7f27dd6bc818a20e4cc8c5a8d3b704ac434
vijmanan04 <75040859+vijmanan04@users.noreply.github.com>
2021-10-17 00:45:24 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -2,17 +2,19 @@ public class Board{
 	final int ROW;
 	final int COL;
 	String board[][];
+	String temp_board[][];
 	public Board(){
 		ROW = 10;
 		COL = 10;
 		board = new String[ROW][COL];
+		temp_board = new String[ROW][COL];
 		for (int i = 0 ; i < ROW; i++){
 				for (int j = 0; j < COL; j++) {
 					board[i][j] = "0";
 				}
 			}
 	}
-	
+
 	public void showBoard(){
 		for (int i = 0 ; i < ROW; i++){
 				for (int j = 0; j < COL; j++) {
@@ -21,28 +23,70 @@ public class Board{
 				System.out.println("");
 		}
 	}
-	
+
 	public void shipLocation(int[] shipCoordinate){
 		int startX = shipCoordinate[0];
 		int startY = shipCoordinate[1];
 		int endX = shipCoordinate[2];
 		int endY = shipCoordinate[3];
-		
-		if (startX == endX){
-			for (int i = startY; i <= endY; i++){
-				board[startX][i] = "+";
-			}
-		if (startY == endY){
-			for (int i = startX; i <= endX; i++){
-				board[i][startY] = "+";
-		}
-			
-		}
-		
-	}
-		
-		
-	}
-	
-}
 
+		boolean checkerX = false;
+		boolean checkerY = false;
+
+		int temp;
+
+		if (startX == endX){
+			if (endY < startY){
+				temp = endY;
+				endY = startY;
+				startY = temp;
+			}
+			for (int i = startY; i <= endY; i++){
+				if (board[startX][i].equals("+")){
+					checkerX = true;
+				}
+			}
+			if (!checkerX){
+				for (int i = startY; i <= endY; i++){
+					board[startX][i] = "+";
+				}
+			}
+		}
+
+		if (startY == endY){
+			if (endX < startX){
+				temp = endX;
+				endX = startX;
+				startX = temp;
+			}
+			for (int i = startX; i <= endX; i++){
+				if (board[i][startY].equals("+")){
+					checkerY = true;
+				}
+			}
+			if (!checkerY) {
+				for (int i = startX; i <= endX; i++){
+					board[i][startY] = "+";
+					}
+				}
+			}
+
+	}
+
+	public void registerAttack(int[] attackCoordinate){
+		int attackX = attackCoordinate[0];
+		int attackY = attackCoordinate[1];
+
+		if (board[attackX][attackY].equals("+")){
+			board[attackX][attackY] = "M";
+			System.out.println("Computer Hits");
+		}
+
+		else{
+			board[attackX][attackY] = "X";
+			System.out.println("Computer Misses");
+		}
+	}
+
+
+}
